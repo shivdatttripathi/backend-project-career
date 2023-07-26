033e6052e199529fc3a257207c09cb8113cf70d6
shivdatttripathi <ishwardatttripathi@gmail.com>
2023-07-26 13:06:59 +0530

    config standard

---
+++ Controller/userController.js
@@ -0,0 +1,4 @@
+async function getApi(req, res) {
+  res.send("Hello World! in controller");
+}
+module.exports = { getApi };
--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,4 +1,6 @@
 const express = require("express");
 const userRouter = express.Router();
+const { getApi } = require("../Controller/userController");
 
+userRouter.get("/getApi", getApi);
 module.exports = userRouter;
