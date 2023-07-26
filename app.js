c269082c6b30210462ba48fd1fa05dd84d9a6e52
shivdatttripathi <ishwardatttripathi@gmail.com>
2023-07-26 12:32:06 +0530

    config data

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const express = require("express");
 // express property transfer in app varible for easy to start server
 const app = express();
 
+// config dotenv
+require("dotenv").config(); //local
+
+//config router
+
+const userRouter = require("./Router/userRouter");
+app.use("/api/user/v1", userRouter);
+
 // start to first get api cheacking working api or not
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+let PORT = process.env.PORT || 4000;
+
 //start server for listen
-app.listen(4000, function () {
-  console.log(`server is $ dev mode mode runnig is port no : $ 4000 `);
+app.listen(PORT, function () {
+  DEV_MODE = "DEVELOPMENT MODE ";
+  DEV_MODE = "DEVELOPMENT MODE ";
+  console.log(`server is ${process.env.DEV_MODE} runnig is port no : ${PORT} `);
 });
